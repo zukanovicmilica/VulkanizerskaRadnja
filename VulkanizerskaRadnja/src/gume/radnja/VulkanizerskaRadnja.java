05fde7dc66e27a72e5bf96005c9335a85afc9452
zukanovicmilica <zukanovic.milica@gmail.com>
2018-03-24 01:20:54 +0100

    Napravljena klasa VulkanizerskaRadnja

---
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -0,0 +1,29 @@
+package gume.radnja;
+
+import java.util.LinkedList;
+
+import gume.AutoGuma;
+
+public class VulkanizerskaRadnja {
+
+	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
+
+	public void dodajGumu(AutoGuma a) {
+		if (a == null)
+			throw new NullPointerException("Guma ne sme biti null");
+		if (gume.contains(a))
+			throw new RuntimeException("Guma vec postoji");
+		gume.addFirst(a);
+	}
+
+	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
+		if (markaModel == null)
+			return null;
+		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
+		for (int i = 0; i < gume.size(); i++)
+			if (gume.get(i).equals(markaModel))
+				novaLista.add(gume.get(i));
+		return novaLista;
+	}
+
+}
